87e8df4b56ae5dee8b713bbb2bb2d0e765687947
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-11-03 13:39:15 +0000

    add

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+type Proxy = {
+  host: string;
+  port: number;
+};
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Welcome to dinosaur API!"));
+app.get("*", async (c) => {
+  const resp = await fetch("https://free-proxy-list.net/");
+  const data = await resp.text();
+  const dom = new DOMParser().parseFromString(data, "text/html");
 
-app.get("/api/", (c) => c.json(data));
-
-app.get("/api/:dinosaur", (c) => {
-  const dinosaur = c.req.param("dinosaur").toLowerCase();
-  const found = data.find((item) => item.name.toLowerCase() === dinosaur);
-  if (found) {
-    return c.json(found);
-  } else {
-    return c.text("No dinosaurs found.");
-  }
+  return c.html(dom.body.outerHTML);
 });
 
 Deno.serve(app.fetch);
